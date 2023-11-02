2e71ab9ccca09b59cc46da8c234cb15724b8fdce
technic404 <126355982+technic404@users.noreply.github.com>
2023-11-02 10:27:29 +0100

    New property
    Added new section window, and property DOMContentLoaded

--- cup.js/utils/public/CjsGlobalsUtil.js
+++ cup.js/utils/public/CjsGlobalsUtil.js
@@ -1,15 +1,19 @@
 /**
  *
- * @type {{mouse: {up: boolean, state: "up"|"down", down: boolean}}}
+ * @type {{mouse: {up: boolean, state: "up"|"down", down: boolean}}, { window: { DOMContentLoaded: boolean } }}
  */
 const CjsGlobals = {
     mouse: {
         up: true,
         down: false,
         state: "up"
+    },
+    window: {
+        DOMContentLoaded: false
     }
 }
 
 /* Mouse */
 window.addEventListener('mousedown', () => { CjsGlobals.mouse = { up: false, down: true, state: "down" } })
 window.addEventListener('mouseup', () => { CjsGlobals.mouse = { up: true, down: false, state: "up" } })
+window.addEventListener('DOMContentLoaded', () => { CjsGlobals.window.DOMContentLoaded = true; })
