17b2424ba0c397a853b669b33da062d7c7ff8d28
technic404 <126355982+technic404@users.noreply.github.com>
2023-11-02 10:25:43 +0100

    Files upload function
    Added function setFiles()

--- cup.js/utils/public/CjsRequestsUtil.js
+++ cup.js/utils/public/CjsRequestsUtil.js
@@ -55,6 +55,31 @@ class CjsRequest {
         this.query = {};
         this.body = {};
         this.headers = {};
+        this.files = {};
+        this.onStartCallback = function() {};
+        this.onEndCallback = function() {};
+    }
+
+    /**
+     *
+     * @param {Function} callback
+     * @returns {CjsRequest}
+     */
+    onStart(callback) {
+        this.onStartCallback = callback;
+
+        return this;
+    }
+
+    /**
+     *
+     * @param {Function} callback
+     * @returns {CjsRequest}
+     */
+    onEnd(callback) {
+        this.onEndCallback = callback;
+
+        return this;
     }
 
     /**
@@ -76,26 +101,43 @@ class CjsRequest {
             xhr.setRequestHeader(key, `${value}`);
         }
 
-        const bodyExists = this.body !== {};
+        const bodyExists = Object.keys(this.body).length > 0;
+        const filesExists = Object.keys(this.files).length > 0;
+
+        if(filesExists && bodyExists) {
+            console.log(`${CJS_PRETTY_PREFIX_X} Cannot send files and body data at the same time`);
+
+            return new CjsRequestResult(0, null, true)
+        }
 
         if(bodyExists) {
             xhr.setRequestHeader("Content-Type", "application/json");
 
             xhr.send(JSON.stringify(this.body));
+        } else if(filesExists) {
+            //xhr.setRequestHeader("Content-Type", "multipart/form-data");
+
+            const formData = new FormData();
+
+            for(const [key, value] of Object.entries(this.files)) {
+                formData.append(key, value)
+            }
+
+            xhr.send(formData);
         } else {
             xhr.send();
         }
 
         xhr.onerror = (e) => {
-            return new CjsRequestResult(
-                0,
-                null,
-                true
-            )
+            return new CjsRequestResult(0, null, true)
         }
 
+        await this.onStartCallback();
+
         return await new Promise(((resolve, reject) => {
-            xhr.onreadystatechange = () => {
+            xhr.onreadystatechange = async () => {
+                this.onEndCallback();
+
                 if(xhr.readyState !== 4) return;
 
                 resolve(
@@ -138,6 +180,17 @@ class CjsRequest {
      */
     setBody(body) {
         this.body = body;
+
+        return this;
+    }
+
+    /**
+     *
+     * @param {Object} files
+     * @returns {CjsRequest}
+     */
+    setFiles(files) {
+        this.files = files;
 
         return this;
     }
