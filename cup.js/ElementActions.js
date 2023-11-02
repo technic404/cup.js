1d783e7f7fbb2c218d1493e9ddfb684780af8ec2
technic404 <126355982+technic404@users.noreply.github.com>
2023-11-02 10:17:08 +0100

    Fixed bugs and added new events

--- cup.js/ElementActions.js
+++ cup.js/ElementActions.js
@@ -36,6 +36,15 @@ function onClick(f) {
 }
 
 /**
+ * Executes when clicked on the element
+ * @param {function(CjsEvent)} f
+ */
+function onInput(f) {
+    return functionMappings.add("input", (event, element, data) => { cjsEventFunction(f, event, element, data); });
+}
+
+
+/**
  * Executes when mouse enter the element
  * @param {function(CjsEvent)} f
  */
@@ -73,6 +82,191 @@ function onOuterclick(f) {
             cjsEventFunction(f, event, element, data);
         }
     }, { windowApplied: true });
+}
+
+// TODO add cancelLeftRightThreshold to function onSlideDown and create function onSlideUp
+
+/**
+ *
+ * @param {function(CjsEvent)} f
+ * @param {Number} slideThreshold
+ * @returns {String}
+ */
+function onSlideDown(f, slideThreshold = 10) {
+    return onLoad((cjsEvent) => {
+        let startY;
+        let lastClientY = null;
+
+        const start = (e) => {
+            const clientY = (!("touches" in e) ? e.clientY : e.touches[0].clientY);
+
+            lastClientY = clientY;
+            startY = clientY;
+        }
+
+        const move = (e) => {
+            const clientY = (!("touches" in e) ? e.clientY : e.touches[0].clientY);
+            const moveProgressed = clientY + 1 >= lastClientY;
+            const deltaY = clientY - startY;
+
+            if(!moveProgressed) {
+                startY = undefined;
+                return;
+            }
+
+            if(deltaY > slideThreshold) {
+                f(cjsEvent)
+
+                startY = undefined;
+            }
+
+            lastClientY = clientY;
+        }
+
+        cjsEvent.source.addEventListener('mousedown', start)
+        cjsEvent.source.addEventListener('touchstart', start)
+
+        cjsEvent.source.addEventListener('mousemove', move);
+        cjsEvent.source.addEventListener('touchmove', move);
+    });
+}
+
+/**
+ *
+ * @param {function(CjsEvent)} f
+ * @param {Number} slideThreshold triggers event when user slides by that amount of pixels
+ * @param {Number} cancelUpDownThreshold cancels event when user slides down or up too much (if disable just set -1)
+ * @returns {String}
+ */
+function onSlideRight(f, slideThreshold = 50, cancelUpDownThreshold = 50) {
+    return onLoad((cjsEvent) => {
+        let mouse = { startX: null, startY: null, lastX: null, lastY: null }
+
+        const start = (e) => {
+            const clientX = (!("touches" in e) ? e.clientX : e.touches[0].clientX);
+            const clientY = (!("touches" in e) ? e.clientY : e.touches[0].clientY);
+
+            mouse.lastX = clientX;
+            mouse.startX = clientX;
+            mouse.lastY = clientY;
+            mouse.startY = clientY;
+        }
+
+        const move = (e) => {
+            const clientX = (!("touches" in e) ? e.clientX : e.touches[0].clientX);
+            const clientY = (!("touches" in e) ? e.clientY : e.touches[0].clientY);
+            const moveProgressed = clientX + 1 >= mouse.lastX;
+            const deltaX = clientX - mouse.startX;
+            const deltaY = clientY - mouse.startY;
+
+            if(cancelUpDownThreshold !== -1 && cancelUpDownThreshold < Math.abs(deltaY)) {
+                mouse.startX = undefined;
+                return;
+            }
+
+            if(!moveProgressed) {
+                mouse.startX = undefined;
+                return;
+            }
+
+            if(deltaX > slideThreshold) {
+                f(cjsEvent)
+
+                mouse.startX = undefined;
+            }
+
+            mouse.lastX = clientX
+        }
+
+        cjsEvent.source.addEventListener('mousedown', start)
+        cjsEvent.source.addEventListener('touchstart', start)
+
+        cjsEvent.source.addEventListener('mousemove', move);
+        cjsEvent.source.addEventListener('touchmove', move);
+    });
+}
+
+/**
+ *
+ * @param {function(CjsEvent)} f
+ * @param {Number} slideThreshold triggers event when user slides by that amount of pixels
+ * @param {Number} cancelUpDownThreshold cancels event when user slides down or up too much (if disable just set -1)
+ * @returns {String}
+ */
+function onSlideLeft(f, slideThreshold = 50, cancelUpDownThreshold = 50) {
+    return onLoad((cjsEvent) => {
+        let mouse = { startX: null, startY: null, lastX: null, lastY: null }
+
+        const start = (e) => {
+            const clientX = (!("touches" in e) ? e.clientX : e.touches[0].clientX);
+            const clientY = (!("touches" in e) ? e.clientY : e.touches[0].clientY);
+
+            mouse.lastX = clientX;
+            mouse.startX = clientX;
+            mouse.lastY = clientY;
+            mouse.startY = clientY;
+        }
+
+        const move = (e) => {
+            const clientX = (!("touches" in e) ? e.clientX : e.touches[0].clientX);
+            const clientY = (!("touches" in e) ? e.clientY : e.touches[0].clientY);
+            const moveProgressed = clientX - 1 <= mouse.lastX;
+            const deltaX = clientX - mouse.startX;
+            const deltaY = clientY - mouse.startY;
+
+            if(cancelUpDownThreshold !== -1 && cancelUpDownThreshold < Math.abs(deltaY)) {
+                mouse.startX = undefined;
+                return;
+            }
+
+            if(!moveProgressed) {
+                mouse.startX = undefined;
+                return;
+            }
+
+            if(deltaX < -1 * slideThreshold) {
+                f(cjsEvent)
+
+                mouse.startX = undefined;
+            }
+
+            mouse.lastX = clientX;
+        }
+
+        cjsEvent.source.addEventListener('mousedown', start)
+        cjsEvent.source.addEventListener('touchstart', start)
+
+        cjsEvent.source.addEventListener('mousemove', move);
+        cjsEvent.source.addEventListener('touchmove', move);
+    });
+}
+
+/**
+ *
+ * @param {function(CjsEvent)} f
+ * @param {Number} time time of hold down in ms
+ * @returns {String}
+ */
+function onHoldDown(f, time = 500) {
+    return onLoad(cjsEvent => {
+        let timeout;
+
+        const cancel = () => { clearTimeout(timeout); }
+        const down = () => {
+            timeout = setTimeout(() => {
+                f(cjsEvent);
+            }, time);
+        }
+
+        cjsEvent.source.addEventListener('mousedown', down);
+        cjsEvent.source.addEventListener('touchstart', down);
+
+        cjsEvent.source.addEventListener('mouseup', cancel);
+        cjsEvent.source.addEventListener('mousemove', cancel);
+        cjsEvent.source.addEventListener('touchend', cancel);
+        cjsEvent.source.addEventListener('touchcancel', cancel);
+        cjsEvent.source.addEventListener('touchmove', cancel);
+    })
 }
 
 /**
